e3a60244dd1d9dc3c53dce9fc8cc1852acc39d49
JJMASTER10 <89151104+JJMater10@users.noreply.github.com>
2023-11-28 19:14:41 -0500

    base de datos

---
+++ iws3.sql
@@ -0,0 +1,379 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 29-11-2023 a las 01:11:10
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `iws3`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cambios_stock`
+--
+
+CREATE TABLE `cambios_stock` (
+  `ID` int(11) NOT NULL,
+  `ID_ProductoServicio` int(11) DEFAULT NULL,
+  `NombreProducto` varchar(255) DEFAULT NULL,
+  `CantidadAntes` int(11) DEFAULT NULL,
+  `CantidadDespues` int(11) DEFAULT NULL,
+  `FechaCambio` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cliente`
+--
+
+CREATE TABLE `cliente` (
+  `ID` int(11) NOT NULL,
+  `NombreEmpresa` varchar(255) DEFAULT NULL,
+  `PersonaContacto` varchar(255) DEFAULT NULL,
+  `Direccion` varchar(255) DEFAULT NULL,
+  `Telefono` varchar(15) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `detallesventa`
+--
+
+CREATE TABLE `detallesventa` (
+  `ID` int(11) NOT NULL,
+  `IDVenta` int(11) DEFAULT NULL,
+  `ID_ProductoServicio` int(11) DEFAULT NULL,
+  `Cantidad` int(11) DEFAULT NULL,
+  `PrecioUnitario` int(45) DEFAULT NULL,
+  `Subtotal` int(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `empleado`
+--
+
+CREATE TABLE `empleado` (
+  `ID` int(11) NOT NULL,
+  `Nombre` varchar(255) DEFAULT NULL,
+  `Apellido` varchar(255) DEFAULT NULL,
+  `Cargo` varchar(50) DEFAULT NULL,
+  `Departamento` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `estado_venta`
+--
+
+CREATE TABLE `estado_venta` (
+  `id` int(45) NOT NULL,
+  `descripcion` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `estado_venta`
+--
+
+INSERT INTO `estado_venta` (`id`, `descripcion`) VALUES
+(1, 'En transito'),
+(2, 'Entregado');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ordencompra`
+--
+
+CREATE TABLE `ordencompra` (
+  `ID` int(11) NOT NULL,
+  `FechaCreacion` date DEFAULT NULL,
+  `ProveedorAsociado` int(11) DEFAULT NULL,
+  `TotalCompra` decimal(10,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `productoservicio`
+--
+
+CREATE TABLE `productoservicio` (
+  `ID` int(11) NOT NULL,
+  `Nombre` varchar(255) DEFAULT NULL,
+  `Descripcion` text DEFAULT NULL,
+  `Precio` int(45) DEFAULT NULL,
+  `CantidadEnStock` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Disparadores `productoservicio`
+--
+DELIMITER $$
+CREATE TRIGGER `after_update_productoservicio` AFTER UPDATE ON `productoservicio` FOR EACH ROW BEGIN
+    -- Verificar si la cantidad en stock ha cambiado
+    IF OLD.CantidadEnStock <> NEW.CantidadEnStock THEN
+        -- Registrar el cambio en la tabla cambios_stock
+        INSERT INTO cambios_stock (ID_ProductoServicio, NombreProducto, CantidadAntes, CantidadDespues)
+        VALUES (NEW.ID, NEW.Nombre, OLD.CantidadEnStock, NEW.CantidadEnStock);
+    END IF;
+END
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `proveedor`
+--
+
+CREATE TABLE `proveedor` (
+  `ID` int(11) NOT NULL,
+  `NombreEmpresa` varchar(255) DEFAULT NULL,
+  `PersonaContacto` varchar(255) DEFAULT NULL,
+  `Direccion` varchar(255) DEFAULT NULL,
+  `Telefono` varchar(15) DEFAULT NULL,
+  `CorreoElectronico` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tiempo_fuera`
+--
+
+CREATE TABLE `tiempo_fuera` (
+  `id` int(45) NOT NULL,
+  `id_empleado_fk` int(45) NOT NULL,
+  `asunto` varchar(225) NOT NULL,
+  `fecha_inicio` datetime NOT NULL,
+  `fecha_final` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `ID` int(11) NOT NULL,
+  `Nombre` varchar(255) DEFAULT NULL,
+  `CorreoElectronico` varchar(255) DEFAULT NULL,
+  `Contraseña` varchar(255) DEFAULT NULL,
+  `Rol` varchar(50) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `venta`
+--
+
+CREATE TABLE `venta` (
+  `ID` int(11) NOT NULL,
+  `FechaVenta` date DEFAULT NULL,
+  `ClienteAsociado` int(11) DEFAULT NULL,
+  `TotalVenta` int(45) DEFAULT NULL,
+  `id_estado_fk` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `cambios_stock`
+--
+ALTER TABLE `cambios_stock`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indices de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indices de la tabla `detallesventa`
+--
+ALTER TABLE `detallesventa`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `IDVenta` (`IDVenta`),
+  ADD KEY `ID_ProductoServicio` (`ID_ProductoServicio`);
+
+--
+-- Indices de la tabla `empleado`
+--
+ALTER TABLE `empleado`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indices de la tabla `estado_venta`
+--
+ALTER TABLE `estado_venta`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `ordencompra`
+--
+ALTER TABLE `ordencompra`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `ProveedorAsociado` (`ProveedorAsociado`);
+
+--
+-- Indices de la tabla `productoservicio`
+--
+ALTER TABLE `productoservicio`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indices de la tabla `proveedor`
+--
+ALTER TABLE `proveedor`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indices de la tabla `tiempo_fuera`
+--
+ALTER TABLE `tiempo_fuera`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_empleado_fk` (`id_empleado_fk`);
+
+--
+-- Indices de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indices de la tabla `venta`
+--
+ALTER TABLE `venta`
+  ADD PRIMARY KEY (`ID`),
+  ADD KEY `ClienteAsociado` (`ClienteAsociado`),
+  ADD KEY `id_estado_fk` (`id_estado_fk`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `cambios_stock`
+--
+ALTER TABLE `cambios_stock`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `detallesventa`
+--
+ALTER TABLE `detallesventa`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `empleado`
+--
+ALTER TABLE `empleado`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `estado_venta`
+--
+ALTER TABLE `estado_venta`
+  MODIFY `id` int(45) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `ordencompra`
+--
+ALTER TABLE `ordencompra`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `productoservicio`
+--
+ALTER TABLE `productoservicio`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `proveedor`
+--
+ALTER TABLE `proveedor`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `tiempo_fuera`
+--
+ALTER TABLE `tiempo_fuera`
+  MODIFY `id` int(45) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `venta`
+--
+ALTER TABLE `venta`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `detallesventa`
+--
+ALTER TABLE `detallesventa`
+  ADD CONSTRAINT `detallesventa_ibfk_1` FOREIGN KEY (`IDVenta`) REFERENCES `venta` (`ID`),
+  ADD CONSTRAINT `detallesventa_ibfk_2` FOREIGN KEY (`ID_ProductoServicio`) REFERENCES `productoservicio` (`ID`);
+
+--
+-- Filtros para la tabla `ordencompra`
+--
+ALTER TABLE `ordencompra`
+  ADD CONSTRAINT `ordencompra_ibfk_1` FOREIGN KEY (`ProveedorAsociado`) REFERENCES `proveedor` (`ID`);
+
+--
+-- Filtros para la tabla `tiempo_fuera`
+--
+ALTER TABLE `tiempo_fuera`
+  ADD CONSTRAINT `tiempo_fuera_ibfk_1` FOREIGN KEY (`id_empleado_fk`) REFERENCES `empleado` (`ID`);
+
+--
+-- Filtros para la tabla `venta`
+--
+ALTER TABLE `venta`
+  ADD CONSTRAINT `venta_ibfk_1` FOREIGN KEY (`ClienteAsociado`) REFERENCES `cliente` (`ID`),
+  ADD CONSTRAINT `venta_ibfk_2` FOREIGN KEY (`id_estado_fk`) REFERENCES `estado_venta` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
