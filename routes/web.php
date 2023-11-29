fcdf6c6b3f1d7fe5c79c64846909fb5e0093a1e0
JJMASTER10 <89151104+JJMater10@users.noreply.github.com>
2023-11-29 16:20:45 -0500

    actualizado con ruta de empleado

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\EmpleadoController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('welcome1');
 });
+
+Route::resource('empleado', EmpleadoController::class);
